3700a370e688b9a433fcac9075eceacab9390351
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-06-03 00:21:24 +0300

    Improve the comment color for "SpringGreen" variation by making the color lighter.

--- generate_variations/SpringGreen.ps1
+++ generate_variations/SpringGreen.ps1
@@ -15,10 +15,10 @@ function Generate-ThemeVariation {
         '"#00CC7E" // type color'
     ).Replace(
         '"#50A750", // comment color',
-        '"#B3B332", // comment color'
+        '"#E3E39C", // comment color'
     ).Replace(
         '"#50A750" // comment color',
-        '"#B3B332" // comment color'
+        '"#E3E39C" // comment color'
     ) | Set-Content $newThemeFile
     # Inform user the theme file was generated:
     # Write-Output "SpringGreen variation generated."
